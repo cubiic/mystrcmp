dff0dbbe5bf08820c201bd55aa01a1faedd43547
Brian Manning <brian@xaoc.org>
2012-03-07 12:02:35 -0800

    added calls to mysplit() from compare()
    - added note in POD that string comparisons are made lexigraphically
    - changed mysplit so it takes key => value arguments instead of just a
      scalar argument
    - added the guts of the comparison loop in compare()

--- perl/MyStrCmp/lib/MyStrCmp.pm
+++ perl/MyStrCmp/lib/MyStrCmp.pm
@@ -44,47 +44,66 @@ sub new {
 =head2 compare ( a => q(string1), b => q(string2) )
 
 Compares two strings "using unsigned characters, so that C<\200> is greater
-than C<\0>" (definition from BSD's C<strcmp(3)>).
+than C<\0>" (definition from BSD's C<strcmp(3)>).  Returns B<-1> if string C<a>
+is lexigraphically less than string C<b>, returns B<0> if both strings match,
+and returns B<1> if string C<a> is lexigraphically greater than string C<b>.
 
 =cut
 
 sub compare {
     my $self = shift;
     my %args = @_;
+    my $strings_match = 0;
 
     die(q|Missing 'a' argument for compare() method|)
         unless ( exists($args{a}) );
     die(q|Missing 'b' argument for compare() method|)
         unless ( exists($args{b}) );
 
-    my @a = $self->mysplit($args{a});
-    my @b = $self->mysplit($args{b});
-
     # - split both strings into arrays so they can be enumerated
+    my @str_a = $self->mysplit(split_string => $args{a});
+    my @str_b = $self->mysplit(split_string => $args{b});
+
     # - check to see if both arguments are greater than zero length
     # strings (i.e. not 'undef')
+    die (qq('a' argument is an empty string)) unless ( scalar(@a) > 0 );
+    die (qq('b' argument is an empty string)) unless ( scalar(@b) > 0 );
+
     # - create a loop
-    # - in the loop, get a substring at character position 'X'
-    # - check to see if either character is undef, i.e. we've reached the end
-    # of the string
-    # - if we have no characters, then we are done, and both strings matched
-    # - if we have two characters, one from each string, compare the two
-    # characters
-    #   - if char a is less than char b, return -1
-    #   - if char a is greater than char b, return 1
+    my $char_counter = 0;
+    while ( ! defined $return_status ) {
+        # - get a substring at character position 'X'
+        my $char_a = $str_a[$char_counter];
+        my $char_b = $str_b[$char_counter];
+
+        # - check to see if either character is undef, i.e. we've reached the
+        # end of the string
+
+        # - if we have no characters, then we are done, and both strings
+        # matched
+        # - if we have two characters, one from each string, compare the two
+        # characters
+        #   - if char a is less than char b, return -1
+        #   - if char a is greater than char b, return 1
+    }
 
     return 0;
 }
 
-=head2 mysplit ( $string )
+=head2 mysplit ( split_string => $string )
 
-Splits the value of C<$string> into individual characters.
+Splits the value of C<$string> into individual characters, and returns them to
+the caller in an array.
 
 =cut
 
 sub mysplit {
     my $self = shift;
-    my $string = shift;
+    my %args = @_;
+
+    die(qq(ERROR: missing string to split as 'split_string'))
+        unless ( exists $args{split_string} );
+    my $string = $args{split_string};
 
     #print qq(mysplit: input string is $string\n);
     my @split;
@@ -92,7 +111,6 @@ sub mysplit {
     my $char;
     # this loop will run as long as $char doesn't receive 'undef'
     while ( $char = substr($string, $counter, 1) ) {
-    #foreach my $char ( substr($string, 0, 1) ) {
         #print qq(mysplit: character $counter of string is: ) . $char . qq(\n);
         $counter++;
         push(@split, $char);
