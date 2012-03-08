61da9d1f4e20eef9e257fdc1a7a75515330d00f0
Brian Manning <brian@xaoc.org>
2012-03-08 07:51:37 -0800

    added files created by Xcode

---
+++ obj-c/MyStrCmp/MyStrCmp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyStrCmp
+//
+//  Created by Brian Manning on 3/8/12.
+//  Copyright (c) 2012 Example Company Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
