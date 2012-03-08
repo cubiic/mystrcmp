80d19fbcda22a803c67d8901381b9be9835e64ce
Brian Manning <brian@xaoc.org>
2012-03-07 22:42:06 -0800

    Basic string comparison works now

--- perl/MyStrCmp/lib/MyStrCmp.pm
+++ perl/MyStrCmp/lib/MyStrCmp.pm
@@ -53,47 +53,80 @@ and returns B<1> if string C<a> is lexigraphically greater than string C<b>.
 sub compare {
     my $self = shift;
     my %args = @_;
-    my $strings_match = 0;
+    # assume both strings are equal until something below changes this
+    my $return_value = 0;
 
     die(q|Missing 'a' argument for compare() method|)
         unless ( exists($args{a}) );
+    my $string_a = $args{a};
     die(q|Missing 'b' argument for compare() method|)
         unless ( exists($args{b}) );
+    my $string_b = $args{b};
 
     # - split both strings into arrays so they can be enumerated
-    my @str_a = $self->mysplit(split_string => $args{a});
-    my @str_b = $self->mysplit(split_string => $args{b});
+    my @str_a = $self->mysplit(split_string => $string_a);
+    my @str_b = $self->mysplit(split_string => $string_b);
 
     # - check to see if both arguments are greater than zero length
     # strings (i.e. not 'undef')
-    die (qq('a' argument is an empty string)) unless ( scalar(@a) > 0 );
-    die (qq('b' argument is an empty string)) unless ( scalar(@b) > 0 );
+    die (qq('a' argument is an empty string)) unless ( scalar(@str_a) > 0 );
+    die (qq('b' argument is an empty string)) unless ( scalar(@str_b) > 0 );
 
     # - create a loop
     my $char_counter = 0;
-    while ( ! defined $return_status ) {
+    # we'll use a 'last' later to get out of this loop
+    while ( 1 ) {
         # - get a substring at character position 'X'
         my $char_a = $str_a[$char_counter];
         my $char_b = $str_b[$char_counter];
 
         # - check to see if either character is undef, i.e. we've reached the
-        # end of the string
-
-        # - if we have no characters, then we are done, and both strings
-        # matched
-        # - if we have two characters, one from each string, compare the two
-        # characters
-        #   - if char a is less than char b, return -1
-        #   - if char a is greater than char b, return 1
+        # end of one or both of the strings
+        if ( defined $char_a ) {
+            if ( defined $char_b ) {
+                # both characters are defined; now see if one character is
+                # less than or greater than the other
+                if ( $char_a lt $char_b ) {
+                    $return_value = -1;
+                    last;
+                } elsif ( $char_a gt $char_b ) {
+                    $return_value = 1;
+                    last;
+                }
+            } else {
+                # $char_a is defined, but $char_b is undefined;
+                # a is greater than b
+                $return_value = 1;
+                last;
+            }
+        } else {
+            if ( defined $char_b ) {
+                # $char_a is undefined, but $char_b is defined;
+                # a is less than b
+                $return_value = -1;
+                last;
+            } else {
+                # both characters undefined, both strings match; exit...
+                last;
+            }
+        }
+        $char_counter++;
     }
 
-    return 0;
+    print qq(Comparison of string A and string B;\n);
+    print qq(string A: $string_a is );
+    if ( $return_value == -1 ) { print q(less than ); }
+    if ( $return_value == 0 ) { print q(equal to ); }
+    if ( $return_value == 1 ) { print q(greater than ); }
+    print qq(string B: $string_b\n);
+
+    return $return_value;
 }
 
 =head2 mysplit ( split_string => $string )
 
 Splits the value of C<$string> into individual characters, and returns them to
-the caller in an array.
+the caller as the contents of an array.
 
 =cut
 
@@ -106,6 +139,7 @@ sub mysplit {
     my $string = $args{split_string};
 
     #print qq(mysplit: input string is $string\n);
+    # hold the contents of the string as an array
     my @split;
     my $counter = 0;
     my $char;
