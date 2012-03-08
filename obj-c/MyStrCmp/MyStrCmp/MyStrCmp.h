9a19b8a9ec0645b8681e6a3e3537d598c33fa6e9
Brian Manning <brian@xaoc.org>
2012-03-08 11:11:12 -0800

    Added MyStrCmp Obj-C class

---
+++ obj-c/MyStrCmp/MyStrCmp/MyStrCmp.h
@@ -0,0 +1,13 @@
+//
+//  MyStrCmp.h
+//  MyStrCmp
+//
+//  Created by Brian Manning on 3/8/12.
+//  Copyright (c) 2012 Example Company Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyStrCmp : NSObject
+
+@end
---
+++ obj-c/MyStrCmp/MyStrCmp/MyStrCmp.m
@@ -0,0 +1,13 @@
+//
+//  MyStrCmp.m
+//  MyStrCmp
+//
+//  Created by Brian Manning on 3/8/12.
+//  Copyright (c) 2012 Example Company Inc. All rights reserved.
+//
+
+#import "MyStrCmp.h"
+
+@implementation MyStrCmp
+
+@end
